3098aaf174d5ebe80b928a569d3b3df69aaecf2c
ranjit-coder123 <ranjitky965@gmail.com>
2026-02-25 15:23:22 +0545

    added new file

---
+++ File_handling/CRUD_operation.py
@@ -0,0 +1,6 @@
+# p = open(r'C:\Users\Lenovo\OneDrive\Desktop\College Info')
+# print(p.read())
+
+
+a = open('')
+print(a.read())
---
+++ Oops/Object.py
@@ -0,0 +1,19 @@
+
+# class Student:
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
+
+#     def show(self):
+#         print(f"Your name is:-{self.name} and age is:-{self.age}")
+
+# obj = Student("Ranjit",22)
+# print(obj.name)
+# obj.show()
+
+  
+    
+A = 20 
+del(A)
+print(A)
+   
